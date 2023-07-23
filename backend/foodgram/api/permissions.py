113fb8114abb26fa2986d5a5b65d9e513deda529
Vladimir Sterliadkin <vs@Vladimirs-MacBook-Pro.local>
2023-07-23 16:52:15 +0700

    Add workflow

--- backend/foodgram/api/permissions.py
+++ backend/foodgram/api/permissions.py
@@ -10,14 +10,3 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
                 or obj.author == request.user)
-
-
-class IsAdminUserOrReadOnly(permissions.BasePermission):
-    """Права доступа для автора либо алминистратора."""
-    def has_permission(self, request, view):
-        return (request.method in permissions.SAFE_METHODS
-                or request.user.is_staff)
-
-    def has_object_permission(self, request, view, obj):
-        return (request.method in permissions.SAFE_METHODS
-                or request.user.is_staff)
--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -10,7 +10,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from api.filters import RecipeFilter
-from api.permissions import IsAuthorOrReadOnly, IsAdminUserOrReadOnly
+from api.permissions import IsAuthorOrReadOnly
 from api.serializers import (IngredientSerializer,
                              RecipeCreateSerializer,
                              RecipeReadSerializer, RecipeSerializer,
@@ -75,7 +75,7 @@ class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
 
 class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
-    permission_classes = [IsAuthorOrReadOnly | IsAdminUserOrReadOnly]
+    permission_classes = (IsAuthorOrReadOnly,)
     filter_backends = (DjangoFilterBackend, )
     filterset_class = RecipeFilter
     http_method_names = ['get', 'post', 'patch', 'create', 'delete']
